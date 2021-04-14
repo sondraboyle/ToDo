f09ab96eb77e40b7718188ffad0682105e0f892b
sondraboyle <sondraboylebim@gmail.com>
2021-04-14 19:10:38 -0400

    got the checkbox functionality

--- functions.js
+++ functions.js
@@ -1,28 +1,31 @@
 
 //Incomplete
 const renderIncomplete = function(array){
-    let completed = array.filter(function(task){
+    let incomplete = array.filter(function(task){
         return !task.completed
     })
     //add to the completed array
-    completed.text = completed
+    incompleteTasks = incomplete
    
-   let completedNum = completed.length //the number of incomplete
+   let incompletedNum = incompleteTasks.length //the number of incomplete
     //create HTML for incomplete
-    document.querySelector('#listTitle').textContent = `Tasks (${completedNum} left)`
+    document.querySelector('#listTitle').textContent = `Tasks (${incompletedNum} left)`
 
-    console.log(completed)
+
     
-    // console.log(completed.length)
+   
 }
 
 
 
 //RENDER DOM
 const renderList = function(list){
+   
+    
 //create new row
 //create unique row id
 let id = list.id
+console.log(id)
 let rowId = taskList.findIndex(function(array){
     return array.id === id
     })
@@ -37,14 +40,41 @@ column.className = "col s1 checkedItem"
 row.appendChild(column)
 
  
-   //input
+   //Checkbox
    const checkbox = document.createElement('input')
    checkbox.setAttribute('type', 'checkbox')
    checkbox.setAttribute('id', `completed${rowId}`)
+   //previously checked stays checked
+   if (taskList[rowId].completed == true ){
+       checkbox.setAttribute('checked', 'checked')
+   }
    checkLabel = document.createElement('label')
    checkLabel.setAttribute('for',`completed${rowId}`)
    column.appendChild(checkbox)
    column.appendChild(checkLabel)
+        //event Listener
+        document.querySelector(`#completed${rowId}`).addEventListener('change', function(e){
+       //get the text element
+      
+       const completedText = e.target.parentNode.parentElement.children[1].innerText
+        //find the index of the matching Todo
+        let textIndex = taskList.findIndex(function(array){
+            return completedText == array.text
+        })
+        console.log(completedText)
+        console.log(taskList)
+        //change completed attribute back
+        if (e.target.checked == true) {
+        taskList[textIndex].completed = true
+        renderIncomplete(taskList)
+       console.log(incompleteTasks)
+        } else {
+        taskList[textIndex].completed = false
+        renderIncomplete(taskList)
+        console.log(incompleteTasks)
+        }
+    
+   })
 
 
 //create second column with text
@@ -76,7 +106,34 @@ row.appendChild(column3)
             link.innerHTML = "<i class='fa fa-remove'></i>" //icon
             document.querySelector( `#column3${rowId}`).appendChild(link);//append the link to the li
 
-    renderIncomplete(taskList)
+                //add event Listener
+                document.querySelector(`#column3${rowId}`).addEventListener('click',function(e){
+                
+                    if(e.target.parentElement.classList.contains('delete-item')){
+
+                        //find the id of the column, turn it to a string, take the last number and find the row with that number in the id
+                        let itemId = JSON.stringify(e.target.parentNode.parentNode.id)
+                        itemIdNum = itemId.slice(8,9)
+                        document.querySelector(`#item${itemIdNum}`).remove()
+                
+                    
+                    //remove this item from the Array as well
+                    let taskId = taskList.findIndex(function (array){
+                        return array.id === id
+                    })
+                    console.log(taskId)
+                    taskList.splice(taskId, 1)
+                    // console.log(taskList)
+                    renderIncomplete(taskList)
+                    
+
+                //make local storage = the new array
+                localStorage.setItem('task', JSON.stringify(taskList))
+                }   
+                })
+
+
+    
  }
 
 
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let taskList = []
 const filter = []
-const completed = []
+let incompleteTasks = []
 
 //check for existing saved data
 const checkSaved = function(list){
@@ -28,6 +28,7 @@ const input =  document.querySelector('#task-form').addEventListener('submit', f
             e.target.elements.task.value = ''
             // console.log(taskList)
             addTask(taskList)
+            renderIncomplete(taskList)
              //save each task added to local storage as a string
              localStorage.setItem('task', JSON.stringify(taskList))
              
@@ -37,45 +38,32 @@ const input =  document.querySelector('#task-form').addEventListener('submit', f
     })
 
 
-//Delete when you press the button --> need to target the whole list, then use an if statement
-document.querySelector('#theList').addEventListener('click',function(e){
-    console.log(e)
-    if(e.target.parentElement.classList.contains('delete-item')){
+// //Delete when you press the button --> need to target the whole list, then use an if statement
+// document.querySelector('#theList').addEventListener('click',function(e){
+   
+//     if(e.target.parentElement.classList.contains('delete-item')){
 
-        //find the id of the column, turn it to a string, take the last number and find the row with that number in the id
-        let itemId = JSON.stringify(e.target.parentNode.parentNode.id)
-        itemIdNum = itemId.slice(8,9)
-        document.querySelector(`#item${itemIdNum}`).remove()
+//         //find the id of the column, turn it to a string, take the last number and find the row with that number in the id
+//         let itemId = JSON.stringify(e.target.parentNode.parentNode.id)
+//         itemIdNum = itemId.slice(8,9)
+//         document.querySelector(`#item${itemIdNum}`).remove()
   
     
-    //remove this item from the Array as well
-   let id = e.target.parentElement.parentElement.id
-    let taskId = taskList.findIndex(function (array){
-        return array.id === id
-    })
-    taskList.splice(taskId, 1)
-    // console.log(taskList)
-    renderIncomplete(taskList)
-
-   //make local storage = the new array
-   localStorage.setItem('task', JSON.stringify(taskList))
-}   
-//checkbox functionality
-else if (e.target.parentElement.classList.contains('checkedItem')){
-    //remove this item from the completed Array 
-const completedText = e.target.parentNode.parentElement.children[1].innerText
-
-//    let itemId = JSON.stringify(e.target.parentNode.parentNode.id)
-//    itemIdNum = itemId.slice(5,6)
-//    completed.splice(itemIdNum, 1)
-//    let taskId = completed.findIndex(function (array){
-//     return array.text === completedText
+//     //remove this item from the Array as well
+//    let id = e.target.parentElement.parentElement.id
+//     let taskId = taskList.findIndex(function (array){
+//         return array.id === id
 //     })
-//     console.log(taskId)
-    console.log(completed)
+//     taskList.splice(taskId, 1)
+//     // console.log(taskList)
+//     renderIncomplete(taskList)
 
-}
-})
+//    //make local storage = the new array
+//    localStorage.setItem('task', JSON.stringify(taskList))
+// }   
+// })
+
+
 
 //CLEAR ALL BUTTON
 document.querySelector('.clear-tasks').addEventListener('click',function(e){
@@ -86,6 +74,7 @@ document.querySelector('.clear-tasks').addEventListener('click',function(e){
   for(let count= taskList.length; count > 0 ; count -- )
     taskList.pop()
     
+    renderIncomplete(taskList)
     
 })
 
